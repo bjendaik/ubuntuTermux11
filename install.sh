5afe00cdbcf3ad35d5e1d0cc08e00e78381aee5f
Bien Huu <107752681+bjendaik@users.noreply.github.com>
2024-06-11 17:18:21 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ finish() {
   local ret=$?
   if [ ${ret} -ne 0 ] && [ ${ret} -ne 130 ]; then
     echo
-    echo "ERROR: Failed to setup XFCE on Termux."
-    echo "Please refer to the error message(s) above"
+    echo "LỖI: Không thể thiết lập XFCE trên Termux."
+    echo "Vui lòng tham khảo (các) thông báo lỗi ở trên"
   fi
 }
 
@@ -18,9 +18,9 @@ trap finish EXIT
 clear
 
 echo ""
-echo "This script will install XFCE Desktop in Termux along with a Debian proot"
+echo "Tập lệnh này sẽ cài đặt XFCE Desktop trong Termux cùng với root Ubuntu"
 echo ""
-read -r -p "Please enter username for proot installation: " username </dev/tty
+read -r -p "Vui lòng nhập tên người dùng để cài đặt root: " username </dev/tty
 
 termux-change-repo
 pkg update -y -o Dpkg::Options::="--force-confold"
@@ -30,10 +30,10 @@ sed -i '12s/^#//' $HOME/.termux/termux.properties
 # Display a message 
 clear -x
 echo ""
-echo "Setting up Termux Storage access." 
+echo "Thiết lập quyền truy cập Bộ lưu trữ Termux." 
 # Wait for a single character input 
 echo ""
-read -n 1 -s -r -p "Press any key to continue..."
+read -n 1 -s -r -p "Bấm phím bất kỳ để tiếp tục..."
 termux-setup-storage
 
 pkgs=('wget' 'ncurses-utils' 'dbus' 'proot-distro' 'x11-repo' 'tur-repo' 'pulseaudio')
@@ -59,10 +59,10 @@ chmod +x *.sh
 # Display a message 
 clear -x
 echo ""
-echo "Installing Termux-X11 APK" 
+echo "Cài đặt Termux-X11 APK" 
 # Wait for a single character input 
 echo ""
-read -n 1 -s -r -p "Press any key to continue..."
+read -n 1 -s -r -p "Bấm phím bất kỳ để tiếp tục..."
 wget https://github.com/termux/termux-x11/releases/download/nightly/app-arm64-v8a-debug.apk
 mv app-arm64-v8a-debug.apk $HOME/storage/downloads/
 termux-open $HOME/storage/downloads/app-arm64-v8a-debug.apk
@@ -73,15 +73,15 @@ termux-reload-settings
 clear -x
 echo ""
 echo ""
-echo "Setup completed successfully!"
+echo "Thiết lập đã hoàn tất thành công!"
 echo ""
-echo "You can now connect to your Termux XFCE4 Desktop to open the desktop use the command start"
+echo "Bây giờ bạn có thể kết nối với Termux XFCE4 Desktop để mở màn hình nền bằng lệnh start"
 echo ""
-echo "This will start the termux-x11 server in termux and start the XFCE Desktop and then open the installed Termux-X11 app."
+echo "Lệnh này sẽ khởi động máy chủ termux-x11 trong termux và khởi động XFCE Desktop, sau đó mở ứng dụng Termux-X11 đã cài đặt."
 echo ""
-echo "To exit, double click the Kill Termux X11 icon on the panel."
+echo "Để thoát, nhấp đúp vào biểu tượng Kill Termux X11 trên bảng điều khiển."
 echo ""
-echo "Enjoy your Termux XFCE4 Desktop experience!"
+echo "Hãy tận hưởng trải nghiệm Termux XFCE4 Desktop của bạn!"
 echo ""
 echo ""
 
